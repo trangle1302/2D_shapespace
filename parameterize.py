612dec224ab88cc24b5e9f492c55c591515873ac
trangle1302 <lengochaitrang@gmail.com>
2021-07-30 10:28:16 +0200

    init parameterize.py with todo functions

---
+++ parameterize.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+from scipy import interpolate as spinterp
+
+def get_interpolators():
+    
+    return coeffs_interpolator, centroids_interpolator, lmax
+
+def get_mapping_coordinates():
+    
+    return coords
+
+def cellular_mapping():
+    
+    return mapping
+
+def morph_representation_on_shape():
+    return img
