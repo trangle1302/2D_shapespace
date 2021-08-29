f874f21627371002eb657d9ccd98b4d44c2d4d38
trangle1302 <lengochaitrang@gmail.com>
2021-08-29 10:11:29 +0200

    delete test

--- parameterize.py
+++ parameterize.py
@@ -182,15 +182,3 @@ def get_interp(coeffs_mem, coeffs_nuc, ):
         lmax: int
     """
     
-    
-from scipy import interpolate
-x = np.arange(-5.01, 5.01, 0.25)
-y = np.arange(-5.01, 5.01, 0.25)
-xx, yy = np.meshgrid(x, y)
-z = np.sin(xx**2+yy**2)
-f = interpolate.interp2d(x, y, z, kind='cubic')
-xnew = np.arange(-5.01, 5.01, 1e-2)
-ynew = np.arange(-5.01, 5.01, 1e-2)
-znew = f(xnew, ynew)
-plt.plot(x, z[0, :], 'ro-', xnew, znew[0, :], 'b-')
-plt.show()
