199099fced9ad39d7907a867157192c907b4cf8f
trangle1302 <lengochaitrang@gmail.com>
2021-10-06 16:23:11 +0200

    get theta and shifts to align protein channel later

--- utils/alignment.py
+++ utils/alignment.py
@@ -2,21 +2,20 @@ import numpy as np
 import pandas as pd
 from skimage.measure import find_contours, regionprops
 from scipy.ndimage import center_of_mass, rotate
-from utils import plotting, helpers, dimreduction, coefs
+from utils import helpers
 import matplotlib.pyplot as plt
 import sys
 from pathlib import Path
-from sklearn.decomposition import PCA
-import scipy
+from imageio import imread
 
-def align_cell_nuclei_centroids(data, plot=False):
+def align_cell_nuclei_centroids(data, protein_ch, plot=False):
     nuclei = data[1, :, :]
     cell = data[0, :, :]
 
     centroid_n = np.rint(center_of_mass(nuclei))
     centroid_c = np.rint(center_of_mass(cell))
     if plot:
-        fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+        fig, ax = plt.subplots(1, 4, figsize=(8, 4))
         ax[0].imshow(nuclei, alpha=0.5)
         ax[0].imshow(cell, alpha=0.5)
         ax[0].plot(
@@ -25,6 +24,7 @@ def align_cell_nuclei_centroids(data, plot=False):
         ax[0].plot(
             [centroid_c[1] + 50, centroid_c[1]], [centroid_c[0], centroid_c[0]], c="b"
         )
+        ax[1].imshow(protein_ch)
 
     cn = [centroid_n[0] - centroid_c[0], centroid_n[1] - centroid_c[1]]
     c0 = [centroid_c[0] - centroid_c[0], centroid_c[1] + 50 - centroid_c[1]]
@@ -33,48 +33,54 @@ def align_cell_nuclei_centroids(data, plot=False):
         theta = 360 - theta
     nuclei = rotate(nuclei, theta)
     cell = rotate(cell, theta)
+    protein_ch = rotate(protein_ch, theta)
 
     if plot:
         centroid_n = np.rint(center_of_mass(nuclei))
         centroid_c = np.rint(center_of_mass(cell))
-        ax[1].imshow(nuclei, alpha=0.5)
-        ax[1].imshow(cell, alpha=0.5)
-        ax[1].plot(
+        ax[2].imshow(nuclei, alpha=0.5)
+        ax[2].imshow(cell, alpha=0.5)
+        ax[2].plot(
             [centroid_c[1], centroid_n[1]], [centroid_c[0], centroid_n[0]], c="r"
         )
-        ax[1].scatter(centroid_c[1], centroid_c[0])
-        ax[1].set_title(f"rotate by {np.round(theta,2)}°")
+        ax[2].scatter(centroid_c[1], centroid_c[0])
+        ax[2].set_title(f"rotate by {np.round(theta,2)}°")
+        ax[3].imshow(protein_ch)
 
-    return nuclei, cell
+    return nuclei, cell, theta
 
 
-def align_cell_major_axis(data, plot=True):
+def align_cell_major_axis(data, protein_ch, plot=True):
     nuclei = data[1, :, :]
     cell = data[0, :, :]
     region = regionprops(cell)[0]
     theta = region.orientation * 180 / np.pi  # radiant to degree conversion
     cell_ = rotate(cell, 90 - theta)
     nuclei_ = rotate(nuclei, 90 - theta)
+    protein_ch_ = rotate(protein_ch, 90-theta)
     if plot:
-        fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+        fig, ax = plt.subplots(1, 4, figsize=(8, 4))
         ax[0].imshow(nuclei, alpha=0.5)
         ax[0].imshow(cell, alpha=0.5)
-        ax[1].imshow(nuclei_, alpha=0.5)
-        ax[1].imshow(cell_, alpha=0.5)
-    return nuclei_, cell_
+        ax[1].imshow(protein_ch)
+        ax[2].imshow(nuclei_, alpha=0.5)
+        ax[2].imshow(cell_, alpha=0.5)
+        ax[3].imshow(protein_ch_)
+    return nuclei_, cell_, 90-theta
 
 
 def get_coefs_df(imlist, n_coef=32, func=None, plot=False):
     coef_df = pd.DataFrame()
+    shifts = dict()
     names = []
     error_n = []
     error_c = []
     for im in imlist:
         data = np.load(im)
-        pro = imageio.imread(Path(str(im).replace('npy', '_protein.png')))
+        pro = imread(Path(str(im).replace('.npy', '_protein.png')))
         try:
-            nuclei, cell = align_cell_nuclei_centroids(data, plot=False)
-            # nuclei, cell = align_cell_major_axis(data, plot=False)
+            nuclei, cell, theta = align_cell_nuclei_centroids(data, pro, plot=False)
+            # nuclei, cell = align_cell_major_axis(data, pro, plot=False)
             centroid = center_of_mass(nuclei)
             # centroid = center_of_mass(cell)
             nuclei_coords_ = find_contours(nuclei)
@@ -89,7 +95,7 @@ def get_coefs_df(imlist, n_coef=32, func=None, plot=False):
             elif max(cell_coords_[:, 0]) < 0 or max(cell_coords_[:, 1]) < 0:
                 print(f"Contour failed {im}")
                 continue
-
+            shifts.update({im: {"theta": theta, "shift_c": centroid}})
             cell_coords = cell_coords_.copy()
             nuclei_coords = nuclei_coords_.copy()
             if plot:
@@ -121,5 +127,5 @@ def get_coefs_df(imlist, n_coef=32, func=None, plot=False):
     print(f"Get coefficients for {len(names)}/{len(imlist)} cells")
     print(f"Reconstruction error for nucleus: {np.average(error_n)}")
     print(f"Reconstruction error for cell: {np.average(error_c)}")
-    return coef_df, names
+    return coef_df, names, shifts
 
