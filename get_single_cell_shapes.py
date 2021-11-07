aceae8eae538475e9d96fe6818b2faec9f806b29
trangle1302 <lengochaitrang@gmail.com>
2021-11-07 20:15:12 +0100

    add keep_cell_list, fix plotitng mistake

--- get_single_cell_shapes.py
+++ get_single_cell_shapes.py
@@ -23,6 +23,7 @@ from utils.helpers import (
 import requests
 from requests.auth import HTTPBasicAuth
 import io
+import glob
 
 
 def plot_complete_mask(json_path):
@@ -32,7 +33,7 @@ def plot_complete_mask(json_path):
         mask["bbox"][3] - mask["bbox"][1] + 1,
     )
     img = np.zeros(img_size)
-    fig, ax = plt.subplot(1, 1, figsize=(20, 20))
+    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
     ax.imshow(img)
     for feature in mask["features"]:
         label = int(feature["properties"]["cell_idx"]) + 1
@@ -40,7 +41,7 @@ def plot_complete_mask(json_path):
         ax.add_patch(PolygonPatch(coords))
     ax.axis("off")
     plt.tight_layout()
-    fig.savefig("C:/Users/trang.le/Desktop/tmp.png", bbox_inches="tight")
+    #fig.savefig("C:/Users/trang.le/Desktop/tmp.png", bbox_inches="tight")
 
     # img = imageio.imread('C:/Users/trang.le/Desktop/tmp.png')
     # plt.imshow(img)
@@ -71,10 +72,12 @@ def get_cell_nuclei_masks(image_id, cell_json):
     return cell_mask2, nuclei_mask, protein
 
 
-def get_single_cell_mask(cell_mask, nuclei_mask, protein, save_path, plot=True):
+def get_single_cell_mask(cell_mask, nuclei_mask, protein, keep_cell_list, save_path, plot=True):
     for region_c, region_n in zip(
         skimage.measure.regionprops(cell_mask), skimage.measure.regionprops(nuclei_mask)
-    ):
+    ):  
+        if region_c.label not in keep_cell_list:
+            continue
         # draw rectangle around segmented cell and
         # apply a binary mask to the selected region, to eliminate signals from surrounding cell
         minr, minc, maxr, maxc = region_c.bbox
@@ -106,15 +109,15 @@ def get_single_cell_mask(cell_mask, nuclei_mask, protein, save_path, plot=True):
             plt.imshow(mask_n, alpha=0.5)
             plt.axis("off")
             plt.tight_layout()
-            # plt.savefig(f"{save_path}{region_c.label}.jpg")
+            plt.savefig(f"{save_path}{region_c.label}.jpg")
 
-        #imageio.imwrite(f"{save_path}{region_c.label}_protein.png", pr)
-        #data = np.stack((mask, mask_n))
-        #np.save(f"{save_path}{region_c.label}.npy", data)
-        #data = np.dstack((mask, np.zeros_like(mask), mask_n)) * 255
-        #imageio.imwrite(f"{save_path}{region_c.label}.png", data)
+        imageio.imwrite(f"{save_path}{region_c.label}_protein.png", pr)
+        data = np.stack((mask, mask_n))
+        np.save(f"{save_path}{region_c.label}.npy", data)
+        data = np.dstack((mask, np.zeros_like(mask), mask_n)) * 255
+        imageio.imwrite(f"{save_path}{region_c.label}.png", data)
         """
-        data = np.expand_dims(data, axis=1)
+        data = np.expand_dims(data, axis=1) 
         data = np.repeat(data, 10, axis=1)
         data = np.swapaxes(data, 2,3) #channel,z,h,w
         np.save(f'{save_path}{region_c.label}.npy', data)
@@ -127,19 +130,22 @@ np.random.seed(42)  # for reproducibility
 
 base_dir = "C:/Users/trang.le/Desktop/annotation-tool"
 base_url = "https://if.proteinatlas.org"
-save_dir = "C:/Users/trang.le/Desktop/2D_shape_space/U2OS_2"
+save_dir = "C:/Users/trang.le/Desktop/2D_shape_space/U2OS"
 # json_path = base_dir + "/HPA-Challenge-2020-all/segmentation/10093_1772_F9_7/annotation_all_ulrika.json"
 df = pd.read_csv(base_dir + "/final_labels_allversions.csv")
 df_test = pd.read_csv(base_dir + "/final_labels_allversions.csv")
 df = df[df.atlas_name == "U-2 OS"]
 imlist = list(set(df.image_id))
 for img_id in imlist:
+    df_img = df[df.image_id == img_id]
+    cell_idx = [int(c.split('/')[1]) for c in df_img.cell_id]
     json_path = max(
         glob.glob(
             f"{base_dir}/HPA-Challenge-2020-all/segmentation/{img_id}/annotation_*"
         ),
         key=os.path.getctime,
     )
+    #plot_complete_mask(json_path)
     cell_mask, nuclei_mask, protein = get_cell_nuclei_masks(img_id, json_path)
     save_path = f"{save_dir}/{img_id}_"
-    get_single_cell_mask(cell_mask, nuclei_mask, protein, save_path)
+    get_single_cell_mask(cell_mask, nuclei_mask, protein, cell_idx, save_path)
