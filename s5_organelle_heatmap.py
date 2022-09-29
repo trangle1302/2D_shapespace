949abb2d594a5477fca2a88f864ae29b02a51285
trangle1302 <lengochaitrang@gmail.com>
2022-09-29 22:32:32 +0200

    rm unused func

--- s5_organelle_heatmap.py
+++ s5_organelle_heatmap.py
@@ -34,31 +34,28 @@ LABEL_TO_ALIAS = {
 
 all_locations = dict((v, k) for k,v in LABEL_TO_ALIAS.items())
 
-def avg_matrix(bins, intensity_matrix):
-    df = []
-    #for 
-    return df 
-
 if __name__ == "__main__":
-    n_coef = 128
-    cell_line = "U-2 OS"
-    save_dir = f"/data/2Dshapespace/{cell_line.replace(' ','_')}/cell_masks"
-    fftcoefs_dir = Path(f"/data/2Dshapespace/{cell_line.replace(' ','_')}/fftcoefs")
-    sampled_intensity_dir = Path(f"/data/2Dshapespace/{cell_line.replace(' ','_')}/sampled_intensity")
+  n_coef = 128
+  cell_line = "U-2 OS"
+  save_dir = f"/data/2Dshapespace/{cell_line.replace(' ','_')}/cell_masks"
+  fftcoefs_dir = Path(f"/data/2Dshapespace/{cell_line.replace(' ','_')}/fftcoefs")
+  sampled_intensity_dir = Path(f"/data/2Dshapespace/{cell_line.replace(' ','_')}/sampled_intensity")
 
-    mappings = pd.read_csv(f"/data/kaggle-dataset/publicHPA_umap/results/webapp/pHPA10000_15_0.1_euclidean_ilsc_2d_bbox_nobordercells.csv")
-    #print(mappings.target.value_counts())
-    print(mappings.columns)
-    id_with_intensity = glob.glob(f"{sampled_intensity_dir}/*.npy")
-    mappings["Link"] =[f"/data/2Dshapespace/U-2_OS/sampled_intensity/{id.split('_',1)[1]}_protein.npy" for id in mappings.id]
-    mappings = mappings[mappings.Link.isin(id_with_intensity)]
-    print(mappings.target.value_counts())
+  mappings = pd.read_csv(f"/data/kaggle-dataset/publicHPA_umap/results/webapp/pHPA10000_15_0.1_euclidean_ilsc_2d_bbox_nobordercells.csv")
+  #print(mappings.target.value_counts())
+  print(mappings.columns)
+  id_with_intensity = glob.glob(f"{sampled_intensity_dir}/*.npy")
+  mappings["Link"] =[f"/data/2Dshapespace/U-2_OS/sampled_intensity/{id.split('_',1)[1]}_protein.npy" for id in mappings.id]
+  mappings = mappings[mappings.Link.isin(id_with_intensity)]
+  print(mappings.target.value_counts())
 
-    f = open('cells_assigned_to_pc_bins.json')
-    cells_assigned = json.load(f)
+  f = open('cells_assigned_to_pc_bins.json')
+  cells_assigned = json.load(f)
 
-    for PC, pc_cells in cells_assigned.items():
-            print("xxxx", len(pc_cells), len(pc_cells[0]))
-            shape = (21,n_coef*2)
-            intensities_pcX = []
-            counts = []
+  for PC, pc_cells in cells_assigned.items():
+    print("xxxx", len(pc_cells), len(pc_cells[0]))
+    shape = (21,n_coef*2)
+    intensities_pcX = []
+    counts = []
+    for org in all_locations.items()[:-1]:
+      intensities = np.load()
