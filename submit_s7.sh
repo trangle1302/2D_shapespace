86c3f6a5c81cc066927ca05adfadd5f5526299f2
trangle1302 <lengochaitrang@gmail.com>
2022-10-16 11:57:18 -0700

    -N to -n

--- submit_s7.sh
+++ submit_s7.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=pc_var
 #
-#SBATCH --time=5:00:00
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=3
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=30G
@@ -13,6 +13,7 @@ module load py-scikit-learn/0.24.2_py36
 module load py-numpy
 module load viz 
 module load py-scikit-image/0.17.2_py36
-srun -N 1 python3 s7_protein_covariance.py --PC 1 
-srun -N 1 python3 s7_protein_covariance.py --PC 3 
-srun -N 1 python3 s7_protein_covariance.py --PC 4
+pip install seaborn
+srun -n 1 python3 s7_protein_covariance.py --PC 1 
+srun -n 1 python3 s7_protein_covariance.py --PC 3 
+srun -n 1 python3 s7_protein_covariance.py --PC 4
