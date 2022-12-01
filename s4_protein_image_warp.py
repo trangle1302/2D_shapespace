394a07dad5cedb0ce0c264d22d811dab9697bc0f
trangle1302 <lengochaitrang@gmail.com>
2022-12-01 06:59:00 +0100

    load cell bin json and find middle bin

--- s4_protein_image_warp.py
+++ s4_protein_image_warp.py
@@ -1,11 +1,14 @@
 import os
-from imageio import imread
+from imageio import imread, imwrite
 import numpy as np
-from utils import coefs, helpers, alignment, image_warp
+from utils import helpers, alignment, image_warp
 import matplotlib.pyplot as plt
 from skimage.morphology import convex_hull_image
-from scipy.ndimage import center_of_mass, rotate
+from scipy.ndimage import rotate
+from skimage.transform import resize
 from utils import TPSpline
+import json
+import pandas as pd
 
 def main():   
     cell_line = 'U-2 OS'
@@ -14,11 +17,13 @@ def main():
     fft_dir = f"{project_dir}/fftcoefs"  
     data_dir = f"{project_dir}/cell_masks" 
     save_dir = f"{project_dir}/U-2_OS/morphed_protein_avg" 
+    plot_dir = f"{project_dir}/U-2_OS/morphed_protein_avg_plots" 
     n_landmarks = 32 # number of landmark points for each ring, so final n_points to compute dx, dy will be 2*n_landmarks+1
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
     # Load average cell
     avg_cell = np.load(f"{shape_mode_path}/Avg_cell.npz")
     nu_centroid = [0,0]
@@ -48,45 +53,65 @@ def main():
     pts_avg = np.vstack([np.asarray(nu_centroid),
                         alignment.realign_contour_startpoint(nu_contour),
                         alignment.realign_contour_startpoint(cell_contour)])
-    print(pts_avg.max(), pts_avg.min())
-    
+    print(pts_avg.max(), pts_avg.min(), cell_contour[:,0].max(), cell_contour[:,1].max())
+    shape_x, shape_y = np.round(cell_contour[:,0].max()).astype('int'), np.round(cell_contour[:,1].max()).astype('int')
+
+    # Loading cell assignation into PC bins
+    f = open(f"{shape_mode_path}/cells_assigned_to_pc_bins.json","r")
+    cells_assigned = json.load(f)
+    #mappings = pd.read_csv("/data/kaggle-dataset/publicHPA_umap/results/umap/sl_pHPA_15_0.05_euclidean_100000_rmoutliers_ilsc_3d_bbox_rm_border.csv")
+    #mappings["cell_idx"] = [idx.split("_",1)[1] for idx in mappings.id]
+    #mappings = mappings[mappings.atlas_name=="U-2 OS"]
+    pc_cells = cells_assigned['PC1']
+    merged_bins = [[0,1,2,3],[4,5,6],[7,8,9,10]]
+    imlist = pc_cells[5]
+    print(imlist[:3])
+
     with open(f"{fft_dir}/shift_error_meta_fft128.txt", "r") as F:
         lines = F.readlines()
-    
-    imlist = []
-    for img_id in imlist:
-        for line in enumerate(lines) :
-            if line.find(img_id) != -1 :
-                vals = line.strip().split(',')
-                break
-        theta = np.float(vals[1])
-        shift_c = (np.float(vals[2].strip('(')),(np.float(vals[3].strip(')'))))
-
-        cell_shape = np.load(f"{data_dir}/{img_id}.npy")
-        img = imread(f"{data_dir}/{img_id}_protein.png")
-        img = rotate(img, theta)
-        print(img.shape)
-        pts_ori = image_warp.find_landmarks(cell_shape[:,:,2], cell_shape[:,:,0], n_points=32, border_points = False)
-        
-        convex_hull_nu = convex_hull_image(cell_shape[:,:,2])
-        convex_hull_cell = convex_hull_image(cell_shape[:,:,0])
-        pts_convex = image_warp.find_landmarks(convex_hull_nu, convex_hull_cell, n_points=32, border_points = False)
-        
-        warped1 = TPSpline.warp_image(pts_ori, pts_convex, img, plot=True, save_dir="")
-        warped = TPSpline.warp_image(pts_convex, pts_avg, img, plot=True, save_dir="")
-        fig, ax = plt.subplots(1,4, figsize=(15,30))
-        ax[0].imshow(cell_shape)
-        ax[0].set_title('original segmentation')
-        ax[1].imshow(img)
-        ax[1].scatter(pts_ori[:,1], pts_ori[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
-        ax[1].set_title('protein channel')
-        ax[2].imshow(warped1)
-        ax[2].scatter(pts_ori[:,1], pts_ori[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
-        ax[2].set_title('ori_shape to midpoint')
-        ax[3].imshow(warped)
-        ax[3].scatter(pts_avg[:,1], pts_avg[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
-        ax[3].set_title('midpoint to avg_shape')
-        fig.savefig(f"{save_dir}/{img_id}.png")
+    for i, bin_ in enumerate(merged_bins):
+        ls = [pc_cells[b] for b in bin_]
+        ls = helpers.flatten_list(ls)
+        ls = [os.path.basename(l).replace(".npy","") for l in ls]
+        # df_sl = mappings[mappings.cell_idx.isin(ls)]
+        print(ls[:3])
+        for img_id in imlist:
+            for line in lines:
+                if line.find(img_id) != -1 :
+                    vals = line.strip().split(',')
+                    break
+            theta = np.float(vals[1])
+            shift_c = (np.float(vals[2].strip('(')),(np.float(vals[3].strip(')'))))
+            print(shift_c)
+            cell_shape = np.load(f"{data_dir}/{img_id}.npy")
+            img = imread(f"{data_dir}/{img_id}_protein.png")
+            img = rotate(img, theta)
+            print(img.shape)
+            img_resized = resize(img, (shape_x, shape_y))
+            cell_shape_resized = resize(cell_shape, (shape_x, shape_y))
+            pts_ori = image_warp.find_landmarks(cell_shape_resized[:,:,2], cell_shape_resized[:,:,0], n_points=32, border_points = False)
+            
+            convex_hull_nu = convex_hull_image(cell_shape_resized[:,:,2])
+            convex_hull_cell = convex_hull_image(cell_shape_resized[:,:,0])
+            pts_convex = image_warp.find_landmarks(convex_hull_nu, convex_hull_cell, n_points=32, border_points = False)
+            
+            warped1 = TPSpline.warp_image(pts_ori, pts_convex, img_resized, plot=False, save_dir="")
+            warped = TPSpline.warp_image(pts_convex, pts_avg, warped1, plot=False, save_dir="")
+            imwrite(warped, f"{save_dir}/{img_id}.png")
+            fig, ax = plt.subplots(1,4, figsize=(15,30))
+            ax[0].imshow(cell_shape, alpha = 0.2)
+            ax[0].imshow(img)
+            ax[0].set_title('original shape+protein')
+            ax[1].imshow(img)
+            ax[1].scatter(pts_ori[:,1], pts_ori[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
+            ax[1].set_title('resized protein channel')
+            ax[2].imshow(warped1)
+            ax[2].scatter(pts_ori[:,1], pts_ori[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
+            ax[2].set_title('ori_shape to midpoint')
+            ax[3].imshow(warped)
+            ax[3].scatter(pts_avg[:,1], pts_avg[:,0], c=np.arange(len(pts_ori)),cmap='Reds')
+            ax[3].set_title('midpoint to avg_shape')
+            fig.savefig(f"{plot_dir}/{img_id}.png")
 
 if __name__ == '__main__':
     main()
