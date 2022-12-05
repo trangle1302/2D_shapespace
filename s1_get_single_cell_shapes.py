2e6c5de4fbf8c0344e2a9aa8f396549c4a999c49
trangle1302 <lengochaitrang@gmail.com>
2022-12-05 22:25:30 +0100

    mv bbox_iou

--- s1_get_single_cell_shapes.py
+++ s1_get_single_cell_shapes.py
@@ -20,7 +20,8 @@ from utils.helpers import (
     geojson_to_masks,
     watershed_lab,
     watershed_lab2,
-    rgb_2_gray_unique
+    rgb_2_gray_unique,
+    bbox_iou
 )
 import requests
 from requests.auth import HTTPBasicAuth
@@ -35,24 +36,6 @@ import cv2
 from skimage.morphology import erosion, square, dilation
 from sklearn.metrics import jaccard_similarity_score
  
-def bbox_iou(boxA, boxB):
-    # determine the (x, y)-coordinates of the intersection rectangle
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
-    # compute the area of intersection rectangle
-    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-    # compute the area of both the prediction and ground-truth
-    # rectangles
-    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
-    iou = interArea / float(boxAArea + boxBArea - interArea)
-    # return the intersection over union value
-    return iou
 
 
 def plot_complete_mask(json_path):
