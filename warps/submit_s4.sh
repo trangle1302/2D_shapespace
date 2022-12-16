aa96f1edfee5ffa41277ed21c47b7ad199e9a869
trangle1302 <lengochaitrang@gmail.com>
2022-12-16 09:59:49 -0800

    rm unused modules

--- warps/submit_s4.sh
+++ warps/submit_s4.sh
@@ -9,13 +9,10 @@
 
 module load python/3.9.0
 module load py-pandas/1.3.1_py39
-#module load py-scikit-learn/0.24.2_py36
 module load py-scipy/1.6.3_py39
 module load py-numpy
-#module load viz 
 module load py-scikit-image/0.19.3_py39
 module load opencv/4.5.2
 pip install imageio
 pip install tqdm
-#pip install more-itertools
 python3 avg_organelle.py
