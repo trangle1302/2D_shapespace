a7fd42cb38ae030850d35b35a7ff4a04ae68d247
trangle1302 <lengochaitrang@gmail.com>
2022-12-16 17:07:43 -0800

    reduce mem request, faster access

--- warps/submit_s4.sh
+++ warps/submit_s4.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=5:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=5G
 
 module load python/3.9.0
 module load py-pandas/1.3.1_py39
