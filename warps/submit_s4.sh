cfa9c3cdbf94f9eb247d0c9cf2d82ecf9701eeec
trangle1302 <lengochaitrang@gmail.com>
2023-02-22 20:45:16 -0800

    update slurm command

--- warps/submit_s4.sh
+++ warps/submit_s4.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name=imagewarp
 #
-#SBATCH --time=20:00:00
-#SBATCH --ntasks=16
+#SBATCH --time=5:00:00
+#SBATCH --ntasks=12
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=5G
 
 module load python/3.9.0
 module load py-pandas/1.3.1_py39
@@ -20,15 +20,10 @@ srun -N 1 -n1 python3 avg_organelle.py --org Nucleoplasm  --pc PC4 &
 srun -N 1 -n1 python3 avg_organelle.py --org Nucleoplasm  --pc PC5 &
 srun -N 1 -n1 python3 avg_organelle.py --org Nucleoplasm  --pc PC6 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC4 &
-srun -N 1 -n1 python3 avg_organelle.py --org GolgiA  --pc PC4 &
-srun -N 1 -n1 python3 avg_organelle.py --org GolgiA  --pc PC5 &
-srun -N 1 -n1 python3 avg_organelle.py --org GolgiA  --pc PC1 &
+srun -N 1 -n1 python3 avg_organelle.py --org Nucleoplasm  --pc PC1 &
+srun -N 1 -n1 python3 avg_organelle.py --org Nucleoplasm  --pc PC2 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC5 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC1 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC2 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC3 &
 srun -N 1 -n1 python3 avg_organelle.py --org VesiclesPCP  --pc PC4 &
-srun -N 1 -n1 python3 avg_organelle.py --org Centrosome --pc PC5 &
-srun -N 1 -n1 python3 avg_organelle.py --org PlasmaM --pc PC4  &
-srun -N 1 -n1 python3 avg_organelle.py --org PlasmaM --pc PC2 &
-srun -N 1 -n1 python3 avg_organelle.py --org PlasmaM --pc PC5  
