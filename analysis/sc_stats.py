644ba7a4aae88a976d34f3b2d96e21b42f3a1316
trangle1302 <lengochaitrang@gmail.com>
2023-03-30 17:15:12 +0200

    stats for hpa images

--- analysis/sc_stats.py
+++ analysis/sc_stats.py
@@ -12,7 +12,7 @@ def check_size(image, shape, d_type = 'uint16', max_val = 65535):
         image = (skimage.transform.resize(image, shape)*max_val).astype(d_type)
     return image
 
-def get_sc_statistics(cell_mask, nuclei_mask, mt, gmnn, cdt1, protein, cell_mask_path):    
+def get_sc_statistics_fucci(cell_mask, nuclei_mask, mt, gmnn, cdt1, protein, cell_mask_path):    
     regions_c = skimage.measure.regionprops(cell_mask)
     regions_n = skimage.measure.regionprops(nuclei_mask)  
 
@@ -63,6 +63,53 @@ def get_sc_statistics(cell_mask, nuclei_mask, mt, gmnn, cdt1, protein, cell_mask
         lines += [line]
     return lines
 
+def get_sc_statistics(cell_mask, nuclei_mask, mt, er, protein, cell_mask_path):    
+    regions_c = skimage.measure.regionprops(cell_mask)
+    regions_n = skimage.measure.regionprops(nuclei_mask)  
+
+    ab_id = cell_mask_path.split('/')[-2]
+    img_id = cell_mask_path.split('/')[-1].replace("_cellmask.png","")
+    lines = []  
+    for region_c in regions_c:
+        # Mask of the cell
+        cell_area = region_c.area
+        minr, minc, maxr, maxc = region_c.bbox
+        mask = cell_mask[minr:maxr, minc:maxc].astype(np.uint8)
+        mask[mask != region_c.label] = 0
+        mask[mask == region_c.label] = 1
+        # protein in the whole cell area
+        pr = protein[minr:maxr, minc:maxc].copy()
+        pr[mask != 1] = 0
+        pr_sum = pr.sum()
+
+        mt_ = mt[minr:maxr, minc:maxc].copy()
+        mt_[mask != 1] = 0
+        mt_sum = mt_.sum()
+        mt_mean = mt_sum/cell_area
+
+        # Mask of the nucleus
+        cell_label = region_c.label
+        region_n = [r for r in regions_n if r.label == cell_label][0]
+        nu_area = region_n.area
+        minr, minc, maxr, maxc = region_n.bbox
+        mask_n = nuclei_mask[minr:maxr, minc:maxc].astype(np.uint8)
+        mask_n[mask_n != region_n.label] = 0
+        mask_n[mask_n == region_n.label] = 1
+        # protein in the nucleus
+        pr_nu = protein[minr:maxr, minc:maxc].copy()
+        pr_nu[mask_n != 1] = 0
+        pr_nu_sum = pr_nu.sum()
+
+        er_ = er[minr:maxr, minc:maxc].copy()
+        er_[mask_n != 1] = 0
+        er_sum = er_.sum()
+        line = ",".join(map(str,[ab_id, img_id+"_"+str(cell_label), # Identifier
+                                 cell_area, nu_area, region_n.eccentricity, # Nucleus and cell area
+                                 pr_sum, pr_nu_sum, # protein total intensity in whole cell and nucleus region, pr_cytosol_mean = (pr_sum-pr_nu)/(cell_area-nu_area)
+                                 mt_sum, er_sum])) + "\n"
+        lines += [line]
+    return lines
+
 def main():
     d = "/data/2Dshapespace/S-BIAD34"
     cell_masks = glob.glob(f"{d}/cell_masks2/*/*_cellmask.png")
@@ -86,7 +133,7 @@ def main():
             protein = imageio.imread( f"{d}/Files/{ab_id}/{img_id}_w4_Rescaled.tif")
             protein = check_size(protein, cell_mask.shape)
 
-            lines = get_sc_statistics(cell_mask, nuclei_mask, mt, gmnn, cdt1, protein, cell_mask_path)
+            lines = get_sc_statistics_fucci(cell_mask, nuclei_mask, mt, gmnn, cdt1, protein, cell_mask_path)
             f.writelines(lines)
     print(f"Finished in {(time.time()-s)/3600}h")
 
