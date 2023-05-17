f04998f0c5dcd0824c3ec717093f78b68cede161
trangle1302 <lengochaitrang@gmail.com>
2023-05-17 10:36:16 -0700

    update run

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
-
+module load python/3.9.0
 pip install snakemake
-snakemake
-#snakemake --cluster "sbatch -t 5:00:00 -p normal -N 1" -j 2
+snakemake --cluster "sbatch -t 25:00:00 -p normal -N 1" -j 1
