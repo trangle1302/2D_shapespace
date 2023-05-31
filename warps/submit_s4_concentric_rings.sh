ff2f7a9d9653d860a7d35f466ece0574f945ea0b
trangle1302 <lengochaitrang@gmail.com>
2023-06-01 01:00:07 +0200

    update module load, arg

--- warps/submit_s4_concentric_rings.sh
+++ warps/submit_s4_concentric_rings.sh
@@ -9,13 +9,15 @@
 
 module load python/3.9.0
 module load py-pandas/1.3.1_py39
+module load py-scikit-image/0.19.3_py39
+module load py-scikit-learn/1.0.2_py39
 pip install imageio
 pip install tqdm
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org HEK_293 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org BJ &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org A-431 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org HEL &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org K562 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org MCF7 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org RT4 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --org U-251_MG 
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line HEK_293 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line BJ &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line A-431 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line HEL &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line K562 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line MCF7 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line RT4 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line U-251_MG 
