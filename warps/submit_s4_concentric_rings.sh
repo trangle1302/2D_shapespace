1e516cf83e2cf633d1583f5c367cb5446b85046d
trangle1302 <lengochaitrang@gmail.com>
2023-06-02 14:24:34 -0700

    update sh

--- warps/submit_s4_concentric_rings.sh
+++ warps/submit_s4_concentric_rings.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name=sampling
 #
-#SBATCH --time=10:00:00
-#SBATCH --ntasks=8
+#SBATCH --time=20:00:00
+#SBATCH --ntasks=5
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10
+#SBATCH --mem-per-cpu=10G
 
 module load python/3.9.0
 module load py-pandas/1.3.1_py39
@@ -14,10 +14,7 @@ module load py-scikit-learn/1.0.2_py39
 pip install imageio
 pip install tqdm
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line HEK_293 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line BJ &
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line A-431 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line HEL &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line K562 &
+srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line K-562 &
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line MCF7 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line RT4 &
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line U-251_MG 
