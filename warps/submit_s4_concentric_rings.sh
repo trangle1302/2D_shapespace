f880aa0b042b17ad96fdc4ff9a5434930954ba58
trangle1302 <lengochaitrang@gmail.com>
2023-06-05 23:30:43 +0200

    update cell lines

--- warps/submit_s4_concentric_rings.sh
+++ warps/submit_s4_concentric_rings.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=sampling
 #
 #SBATCH --time=20:00:00
-#SBATCH --ntasks=5
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10G
 
@@ -13,8 +13,5 @@ module load py-scikit-image/0.19.3_py39
 module load py-scikit-learn/1.0.2_py39
 pip install imageio
 pip install tqdm
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line HEK_293 &
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line A-431 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line K-562 &
-srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line MCF7 &
 srun -N 1 -n1 python3 s4_concentric_rings_intensity.py --cell_line U-251_MG 
