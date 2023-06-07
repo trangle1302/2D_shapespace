d14416efa08bc96a6a1d226fc22590dea825671f
trangle1302 <lengochaitrang@gmail.com>
2023-06-08 00:51:12 +0200

    generalize org heatmap

--- analysis/organelle_heatmap.py
+++ analysis/organelle_heatmap.py
@@ -2,35 +2,10 @@ import os
 import numpy as np
 import pandas as pd
 from pathlib import Path
-import argparse
 import matplotlib.pyplot as plt
 from imageio import imread, imwrite
 from skimage.metrics import structural_similarity
 from warps import parameterize
-
-LABEL_TO_ALIAS = {
-    0: "Nucleoplasm",
-    1: "NuclearM",
-    2: "Nucleoli",
-    3: "NucleoliFC",
-    4: "NuclearS",
-    5: "NuclearB",
-    6: "EndoplasmicR",
-    7: "GolgiA",
-    8: "IntermediateF",
-    9: "ActinF",
-    10: "Microtubules",
-    # 11: 'MitoticS',
-    12: "Centrosome",
-    13: "PlasmaM",
-    14: "Mitochondria",
-    # 15: 'Aggresome',
-    16: "Cytosol",
-    17: "VesiclesPCP",
-    # 18: 'Negative',
-    # 19:'Multi-Location',
-}
-
 
 def correlation(value_dict, method_func):
     cor_mat = np.zeros((len(value_dict), len(value_dict)))
@@ -40,31 +15,63 @@ def correlation(value_dict, method_func):
     return cor_mat
 
 
+def unmerge_label(
+    mappings_df,
+    merged_label="VesiclesPCP",
+    subcomponents=[
+        "Lipid droplets",
+        "Endosomes",
+        "Lysosomes",
+        "Peroxisomes",
+        "Vesicles",
+        "Cytoplasmic bodies",
+    ],
+):
+    mappings_df["sc_locations"] = ""
+    mappings_df["sc_target"] = ""
+    for i, r in mappings_df.iterrows():
+        if r.target == merged_label:
+            sc_l = [l for l in r.locations.split(",") if l in subcomponents]
+            mappings_df.loc[i, "sc_locations"] = ",".join(sc_l)
+            if len(sc_l) > 1:
+                mappings_df.loc[i, "sc_target"] = "Multi-Location"
+            else:
+                mappings_df.loc[i, "sc_target"] = sc_l[0]
+        else:
+            mappings_df.loc[i, "sc_locations"] = r.target
+            mappings_df.loc[i, "sc_target"] = r.target
+    return mappings_df
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--org", help="Organelle class", type=str)
-    args = parser.parse_args()
-    print(args.org)
+    import configs.config as cfg
+    log_dir = f"{cfg.PROJECT_DIR}/logs"
+    fft_dir = f"{cfg.PROJECT_DIR}/fftcoefs/{cfg.ALIGNMENT}"
+    fft_path = os.path.join(fft_dir, f"fftcoefs_{cfg.N_COEFS}.txt")
+    shape_mode_path = f"{cfg.PROJECT_DIR}/shapemode/{cfg.ALIGNMENT}"
+    avg_organelle_dir = f"{cfg.PROJECT_DIR}/matrix_protein_avg"
 
-    n_coef = 128
-    cell_line = "U-2 OS"
-    project_dir = f"/scratch/users/tle1302/2Dshapespace/{cell_line.replace(' ','_')}"
-    log_dir = f"{project_dir}/logs"
-    fftcoefs_dir = f"{project_dir}/fftcoefs/fft_major_axis_polarized"
-    fft_path = os.path.join(fftcoefs_dir, f"fftcoefs_{n_coef}.txt")
-    shape_mode_path = (
-        f"{project_dir}/shapemode/{cell_line.replace(' ','_')}/fft_major_axis_polarized"
-    )
-    avg_organelle_dir = f"{project_dir}/morphed_protein_avg"
-
-    merged_bins = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
+    cellline_meta = os.path.join(cfg.PROJECT_DIR, os.path.basename(cfg.META_PATH).replace(".csv", "_splitVesiclesPCP.csv"))
+    if os.path.exists(cellline_meta):
+        mappings = pd.read_csv(cellline_meta)
+    else:
+        mappings = pd.read_csv(cfg.META_PATH)
+        mappings = mappings[mappings.atlas_name == cfg.CELL_LINE]
+        mappings["cell_idx"] = [idx.split("_", 1)[1] for idx in mappings.id]
+        mappings = unmerge_label(mappings)
+        mappings.to_csv(cellline_meta, index=False)
+        
+    merged_bins = [[0], [1], [2], [3], [4], [5], [6]]
     # Panel 1: Organelle through shapespace
-    for PC in np.arange(8):
-        for org in LABEL_TO_ALIAS.values():
+    for PC in np.arange(6):
+        for org in cfg.LABEL_TO_ALIAS.values():
             for i, bin_ in enumerate(merged_bins):
+                intensities = []
                 if len(bin_) == 1:
                     bin_ = bin_[0]
-                    org_bin = imread(f"{avg_organelle_dir}/PC{PC}/bin_{bin_}_{org}")
+                    org_bin = imread(f"{avg_organelle_dir}/PC{PC}/bin{bin_}_{org}.png")
+                    
+            intensities = []
 
     # Panel 2: Organelle heatmap through shapespace
     for PC in np.arange(8):
@@ -74,8 +81,8 @@ if __name__ == "__main__":
                 if len(bin_) == 1:
                     b = bin_[0]
                     images = {}
-                    for org in LABEL_TO_ALIAS.values():
-                        images[org] = imread(f"{d}/{PC}/bin{b}_{org}.png")
+                    for org in cfg.ORGANELLES:
+                        images[org] = imread(f"{avg_organelle_dir}/{PC}/bin{b}_{org}.png")
 
             ssim_scores = correlation(images, structural_similarity)
             ssim_df = pd.DataFrame(ssim_scores, columns=list(images.keys()))
