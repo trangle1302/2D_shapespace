4542b6001638903757f4d8899582993e194e69a4
trangle1302 <lengochaitrang@gmail.com>
2023-06-08 19:43:34 +0200

    sh script

--- analysis/submit_s5.sh
+++ analysis/submit_s5.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=organelle
 #
 #SBATCH --time=5:00:00
-#SBATCH --ntasks=11
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=10G
 
 module load python/3.6.1
 module load py-pandas/1.0.3_py36
@@ -15,14 +15,11 @@ module load viz
 module load py-scikit-image/0.17.2_py36
 pip install tqdm
 pip install more-itertools
-srun -n 1 python3 s5_organelle_heatmap.py --org Nucleoplasm
-srun -n 1 python3 s5_organelle_heatmap.py --org NuclearM
-srun -n 1 python3 s5_organelle_heatmap.py --org Nucleoli
-srun -n 1 python3 s5_organelle_heatmap.py --org NucleoliFC
-srun -n 1 python3 s5_organelle_heatmap.py --org NuclearS
-srun -n 1 python3 s5_organelle_heatmap.py --org NuclearB
-srun -n 1 python3 s5_organelle_heatmap.py --org Microtubules
-srun -n 1 python3 s5_organelle_heatmap.py --org MitoticS
-srun -n 1 python3 s5_organelle_heatmap.py --org Centrosome
-srun -n 1 python3 s5_organelle_heatmap.py --org PlasmaM
-srun -n 1 python3 s5_organelle_heatmap.py --org Cytosol
+srun -n 1 python3 organelle_heatmap.py --cell_line A-431
+srun -n 1 python3 organelle_heatmap.py --cell_line HEK_293
+srun -n 1 python3 organelle_heatmap.py --cell_line MCF7
+srun -n 1 python3 organelle_heatmap.py --cell_line U-251_MG
+srun -n 1 python3 organelle_heatmap.py --cell_line HEL
+srun -n 1 python3 organelle_heatmap.py --cell_line BJ
+srun -n 1 python3 organelle_heatmap.py --cell_line K-562
+srun -n 1 python3 organelle_heatmap.py --cell_line RT4
