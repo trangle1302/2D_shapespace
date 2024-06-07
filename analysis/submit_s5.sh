198802c9c4213457eae2cb7d723275e70fb86cec
trangle1302 <lengochaitrang@gmail.com>
2024-06-07 16:19:48 -0700

    update jobs, cleanup

--- analysis/submit_s5.sh
+++ analysis/submit_s5.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --job-name=organelle
 #
-#SBATCH --time=5:00:00
-#SBATCH --ntasks=8
+#SBATCH --time=25:00:00
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10G
 
@@ -14,11 +14,15 @@ module load viz
 module load py-scikit-image/0.17.2_py36
 pip install imageio
 pip install seaborn
-srun -n 1 python3 organelle_heatmap.py --cell_line A-431
-srun -n 1 python3 organelle_heatmap.py --cell_line HEK_293
-srun -n 1 python3 organelle_heatmap.py --cell_line MCF7
-srun -n 1 python3 organelle_heatmap.py --cell_line U-251_MG
-srun -n 1 python3 organelle_heatmap.py --cell_line HEL
-srun -n 1 python3 organelle_heatmap.py --cell_line K-562
-srun -n 1 python3 organelle_heatmap.py --cell_line RT4
-#srun -n 1 python3 organelle_heatmap.py --cell_line BJ
+#python3 organelle_heatmap.py --cell_line A-431
+#python3 organelle_heatmap.py --cell_line HEK_293
+#python3 organelle_heatmap.py --cell_line MCF7
+#python3 organelle_heatmap.py --cell_line BJ
+#python3 organelle_heatmap.py --cell_line K-562
+#python3 organelle_heatmap.py --cell_line RT4
+python3 organelle_heatmap.py --cell_line U-251_MG
+#python3 organelle_heatmap.py --cell_line HEL
+#python3 organelle_CM_heatmap.py --cell_line RT4
+#python3 organelle_CM_heatmap.py --cell_line BJ
+#python3 organelle_CM_heatmap.py --cell_line U-251_MG
+#python3 organelle_CM_heatmap.py --cell_line HEL
