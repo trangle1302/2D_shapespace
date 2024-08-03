9aacd1c10f768f04f35f1b83d483d21e9e36aec3
trangle1302 <lengochaitrang@gmail.com>
2024-08-02 22:46:29 -0700

    segment B2AI

--- segmentation/s0_cellpose2_prediction_tmp.py
+++ segmentation/s0_cellpose2_prediction_tmp.py
@@ -31,24 +31,23 @@ def predict(model_path, files, plot_dir, diameter=0):
     # use model diameter if user diameter is 0
     diameter = model.diam_labels if diameter == 0 else diameter
     print(f"object diameter: {diameter}, threshold at {diameter*0.8}")
-    diameter = diameter * 0.8
+    diameter = diameter * 1.5
     # gets image files in dir (ignoring image files ending in _masks)
     cellprob_threshold = 0
     model_name = model_path.rsplit("_")[1]
     if model_name == "nuclei":
-        flow_threshold = 0.1
+        flow_threshold = 0
         channels = [2, 3]
 
         def read_img(f):
-            w1 = io.imread(f)
-            w2 = io.imread(f.replace("w1.tif", "w2.tif"))
-            w3 = io.imread(f.replace("w1.tif", "w3.tif"))
+            w4 = io.imread(f)
+            w0 = io.imread(f.replace("C4.tif", "C0.tif"))
+            w1 = io.imread(f.replace("C4.tif", "C1.tif"))
+            #print(f'read {w1.max(), w0.max(), w4.max()}')
             try:
-                if w1.max() > 0 and w2.max() > 0 and w3.max() > 0:
-                    # img = np.stack([sharpen(w1),sharpen(w2), sharpen(w3)])
-                    # w23 = adaptive_hist(w2) + adaptive_hist(w3)
-                    # img = np.stack([sharpen(w1),adaptive_hist(w23), sharpen(w1)])
-                    img = np.stack([sharpen(w1), adaptive_hist(w2), adaptive_hist(w3)])
+                if w1.max() > 0 and w0.max() > 0 and w4.max() > 0:
+                    w0 = sharpen(w0) # rescale intensity
+                    img = np.stack([adaptive_hist(w0), adaptive_hist(w0), adaptive_hist(w0)])
                 else:  # fail because empty channel
                     img = []
             except:  # fail because reading error
@@ -61,12 +60,12 @@ def predict(model_path, files, plot_dir, diameter=0):
 
         def read_img(f):
             w1 = io.imread(f)
-            nuclei = io.imread(f.replace("w1.tif", "nucleimask.png"))
+            nuclei = io.imread(f.replace("C4.tif", "nucleimask.png"))
             nuclei = nuclei / nuclei.max()
             img = np.stack([np.zeros_like(w1), sharpen(w1), nuclei])
             return img
 
-    chunk_size = 10
+    chunk_size = 64
     n = len(files)
     with tqdm(total=n) as pbar:
         for start_ in range(0, n, chunk_size):
@@ -77,10 +76,10 @@ def predict(model_path, files, plot_dir, diameter=0):
                 img = read_img(files[i_])
                 if len(img) == 0:
                     with open(
-                        "/data/2Dshapespace/S-BIAD34/resegmentation/failed_imgs_channelvalue0.txt",
+                        "/scratch/users/tle1302/2Dshapespace/B2AI/failed_imgs_channelvalue0.txt",
                         "a",
                     ) as f:
-                        print(f'Failed: {files[i_].split("/")[-2:]}')
+                        print(f'Failed: {files[i_].split("/")[-3:]}')
                         f.write(files[i_])
                 else:
                     images += [img]
@@ -99,74 +98,75 @@ def predict(model_path, files, plot_dir, diameter=0):
 
                 if True:
                     i = np.random.choice(len(images))
-                    # for i in range(1, len(images)):
-                    fig = plt.figure(figsize=(40, 10), facecolor="black")
-                    name = os.path.basename(file_names[i]).replace("_w1.tif", ".png")
-                    name = "_".join([file_names[i].split("/")[-2], name])
-                    print(name)
-                    img = images[i].copy()
-                    if img.shape[0] != len(channels):
-                        tmp = []
-                        for ch in channels:
-                            tmp += [img[ch - 1]]
-                        img = np.stack(tmp) * 0.3
-                    plot.show_segmentation(
-                        fig,
-                        img,
-                        masks[i],
-                        flows[i][0],
-                        channels=channels,
-                        file_name=None,
-                    )
-                    fig.savefig(f"{plot_dir}/{name}")
-                    plt.close()
+                    #for i in range(1, len(images)):
+                    if True:
+                        fig = plt.figure(figsize=(40, 10), facecolor="black")
+                        name = os.path.basename(file_names[i]).replace(".tif", ".png")
+                        name = "_".join([file_names[i].split("/")[-3], file_names[i].split("/")[-2], name])
+                        img = images[i].copy()
+                        if img.shape[0] != len(channels):
+                            tmp = []
+                            for ch in channels:
+                                tmp += [img[ch - 1]]
+                            img = np.stack(tmp) #* 0.3
+                        plot.show_segmentation(
+                            fig,
+                            img,
+                            masks[i],
+                            flows[i][0],
+                            channels=channels,
+                            file_name=None,
+                        )
+                        print(f'savng : {plot_dir}/{name}')
+                        fig.savefig(f"{plot_dir}/{name}")
+                        plt.close()
                     # io.imsave(f'{plot_dir}/{name[:-4]}_{model_name}mask.png',masks[i])
             for m, f in zip(masks, file_names):
-                io.imsave(f.replace("w1.tif", f"{model_name}mask.png"), m)
-            pbar.update(end_ - start_)
+                io.imsave(f.replace("C4.tif", f"{model_name}mask.png"), m)
+        pbar.update(end_ - start_)
 
+def delete_empty_lowcount(folders):
+    for f in folders:
+        for ch in ['C0.tif','C1.tif','C2.tif','C3.tif','C4.tif']:
+            img = io.imread(f'{f}/{ch}')
+            if img.max() < 100:
+                print(f'Removing {f}/{ch}')
+                os.remove(f'{f}/{ch}')
 
 if __name__ == "__main__":
-    base_dir = "/data/2Dshapespace/S-BIAD34"
+    base_dir = "/scratch/users/tle1302/2Dshapespace/B2AI/MDA-MB-468/Tiffs/B2AI-2023-1/"
+    
     if False:
-        files = natsorted(glob(f"{base_dir}/resegmentation/train/*w1.tif"))
-        files = [
-            f
-            for f in files
-            if not os.path.exists(f.replace("w1.tif", "nucleimask.png"))
-        ]
-        print(f"==========> Segmenting nucleus")
-        os.makedirs(f"{base_dir}/resegmentation/QCs/nuclei", exist_ok=True)
-        predict(
-            model_path=f"{base_dir}/resegmentation/models/S-BIAD34_nuclei",
-            files=files,
-            plot_dir=f"{base_dir}/resegmentation/QCs/nuclei",
-        )
-
-    if False:
-        # files_finished = natsorted(glob(f'{base_dir}/Files/*/*nucleimask.png'))
-        # files_finished = [f.replace('nucleimask.png','w1.tif') for f in files_finished]
-        # print(f'Found {len(files_finished)} FOVs with nucleimasks.png done')
-        files = natsorted(glob(f"{base_dir}/Files/*/*w1.tif"))
-        # files = [f for f in files if f not in files_finished]
+        folders = natsorted(glob(f"{base_dir}/*/*/*"))
+        delete_empty_lowcount(folders)
+        
+    if True:
+        files_finished = natsorted(glob(f"{base_dir}/*/*/*/*nucleimask.png"))
+        files_finished = [f.replace('nucleimask.png','C4.tif') for f in files_finished]
+        print(f'Found {len(files_finished)} FOVs with nucleimasks.png done')
+        files = natsorted(glob(f"{base_dir}/*/*/*/C4.tif")) # w4 is microtubules
+        files = [f for f in files if f not in files_finished]
         print(f"========== Segmenting {len(files)} fovs ==========")
 
         print(f"==========> Segmenting nucleus")
-        os.makedirs(f"{base_dir}/resegmentation/QCs/nuclei", exist_ok=True)
+        os.makedirs(f"/scratch/users/tle1302/2Dshapespace/QCs/nuclei", exist_ok=True)
         predict(
-            model_path=f"{base_dir}/resegmentation/models/S-BIAD34_nuclei",
+            model_path=f"/scratch/users/tle1302/2Dshapespace/models/S-BIAD34_nuclei",
             files=files,
-            plot_dir=f"{base_dir}/resegmentation/QCs/nuclei",
+            plot_dir=f"/scratch/users/tle1302/2Dshapespace/QCs/nuclei",
         )
 
     if True:
-        files = natsorted(glob(f"{base_dir}/Files/*/*nucleimask.png"))
-        files = [f.replace("nucleimask.png", "w1.tif") for f in files]
+        files_finished = natsorted(glob(f"{base_dir}/*/*/*/*cytomask.png"))
+        files_finished = [f.replace('nucleimask.png','C4.tif') for f in files_finished]
+        files = natsorted(glob(f"{base_dir}/*/*/*/nucleimask.png"))
+        files = [f.replace("nucleimask.png", "C4.tif") for f in files]
+        files = [f for f in files if f not in files_finished]
         print(f"========== Segmenting {len(files)} fovs ==========")
         print(f"==========> Segmenting cells")
-        os.makedirs(f"{base_dir}/resegmentation/QCs/cell", exist_ok=True)
+        os.makedirs(f"/scratch/users/tle1302/2Dshapespace/QCs/cell", exist_ok=True)
         predict(
-            model_path=f"{base_dir}/resegmentation/models/S-BIAD34_cyto",
+            model_path=f"/scratch/users/tle1302/2Dshapespace/models/S-BIAD34_cyto",
             files=files,
-            plot_dir=f"{base_dir}/resegmentation/QCs/cell",
+            plot_dir=f"/scratch/users/tle1302/2Dshapespace/QCs/cell",
         )
